3e41af16e082a378b6da28362047d8b00f7f9582
DxKwanju <nnn1520@deltax.ai>
2025-06-24 09:47:23 +0900

    feat:bank account number

--- Solution #1/assignment_1.py
+++ Solution #1/assignment_1.py
@@ -8,17 +8,21 @@ and the account type is considered when depositing and withdrawing.
 """
 # Make a class for a bank account. The class contains the following attributes
 from datetime import datetime
+import random
+
 class BankAccount:
     Unique_id = 1
     def __init__(self, username, password, account_type, interest_rate):
         self.__username = username
         self.__password = password
+        self.__bank_account_number = random.randint(10000000, 99999999)
         self.__account_type = account_type
         self.__interest_rate = interest_rate
         self.__balance = 0
         self.__created_date = datetime.now()
         self.__id = BankAccount.Unique_id
         BankAccount.Unique_id += 1
+        
         
     def __del__(self):
         """Finalizer to be called when an account object is destroyed."""
@@ -62,7 +66,7 @@ class BankAccount:
             else:
                 print("Saving_account cannot withdraw before 1 year")
                 
-        elif self.__account_type=="minus_account":
+        elif self.__account_type=="Minus_account":
             # Check if withdrawal would exceed the -500,000 limit
             if self.__balance - amount >= -500000:
                 self.__balance -= amount
@@ -80,6 +84,7 @@ class BankAccount:
         print(f"Interest rate: {self.__interest_rate}")
         print(f"Balance: {self.__balance}")
         print(f"Created date: {self.__created_date}")
+        print(f"Bank account number: {self.__bank_account_number}")
         
     def get_balance(self):
         return self.__balance
@@ -95,7 +100,7 @@ if __name__ == "__main__":
     account2.withdraw(50000, "123456")
     account2.show_account_info()
     print("--------------------------------"*2)
-    account3 = BankAccount("Jim", "123456", "minus_account", 0.01)
+    account3 = BankAccount("Jim", "123456", "Minus_account", 0.01)
     account3.deposit(100000)
     account3.withdraw(150000, "123456")
     account3.show_account_info()
